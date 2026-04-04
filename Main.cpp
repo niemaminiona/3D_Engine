afedafae05d205651b4416b6bf22216b4be053e9
niemaminiona <f11w@o2.pl>
2026-04-04 12:56:03 +0200

    window now shows up

--- Main.cpp
+++ Main.cpp
@@ -3,5 +3,29 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+	
+	if (!glfwInit()) {
+		std::cout << "Failed to intialize GLFW" << std::endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800,800, "OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
