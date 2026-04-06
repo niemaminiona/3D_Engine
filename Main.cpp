5db793408821733ae05461db0bb8a6e7f2792976
niemaminiona <f11w@o2.pl>
2026-04-06 15:33:46 +0200

    added shaders, also added file reading with shader reading

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,12 @@
 #define GLFW_INCLUDE_NONE
 #include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using string = std::string;
 
 // classic full_HD 16:9 resolution
 int windowWidth = 1920;
@@ -18,9 +20,41 @@ void checkClose(GLFWwindow* window) {
 	}
 }
 
+// function that reads data from files and return it in string
+string static readFromFile(const string filepath) {
+	std::ifstream file(filepath);
+
+	if (!file.is_open()) {
+		std::cout << "Failed to open file: " << filepath << std::endl;
+		return "";
+	}
+	string line, content;
+	while (std::getline(file, line)) {
+		content += line + "\n";
+	}
+
+	return content;
+}
+
+const char* readShader(const string filepath) {
+	static string source;
+	source = readFromFile(filepath);
+	if (source.empty()) {
+		std::cout << "File is empty: " + filepath << std::endl;
+	}
+	return source.c_str();
+}
+
+
+// Vertex Shader source code
+const char* vertexShaderSource = readShader("Shaders/exampleShader1/vertex_shader.glsl");
+//Fragment Shader source code
+const char* fragmentShaderSource = readShader("Shaders/exampleShader1/fragment_shader.glsl");
+
+
 // main program
 int main() {
-	
+	std::cout << readFromFile("Shaders/exampleShader1/vertex_shader.glsl") << std::endl;
 
 	if (!glfwInit()) {
 		std::cout << "Failed to intialize GLFW" << std::endl;
@@ -49,11 +83,8 @@ int main() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	
-
-
-
 	int framesCount = 0;
-	ShowWindow(GetConsoleWindow(), SW_HIDE); // hides terminal from background
+	//ShowWindow(GetConsoleWindow(), SW_HIDE); // hides terminal from background
 	// main update loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -61,7 +92,7 @@ int main() {
 
 
 		framesCount++;
-		std::string title = "OpenGL " + std::to_string(framesCount);
+		string title = "OpenGL " + std::to_string(framesCount);
 		glfwSetWindowTitle(window, title.c_str());
 
 		glfwSwapBuffers(window);
